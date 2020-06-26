c128beb0c8086fdab8c4581155a4cace651aac09
David <twdeveloper@hotmail.com>
2020-06-26 17:03:59 +0800

    frist draft

--- Program.cs
+++ Program.cs
@@ -1,16 +1,170 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
 
 namespace SnagitShare2Imgur
 {
     class Program
     {
+        public class Options
+        {
+            [Option('i', "ImgurClientID", Required = false, HelpText = "Imgur App ClientID")]
+            public string ImgurClientID { get; set; }
+
+
+            [Option('f', "File", Required = false, HelpText = "File Path to Upload")]
+            public string FilePath { get; set; }
+
+            //public string GetUsage()
+            //{
+            //    return CommandLine.Text.HelpText.AutoBuild(this,
+            //      (CommandLine.Text.HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            //}
+        }
+
+        static void RunOptions(Options o)
+        {
+            //there is no any argument
+            if (_args.Length <= 0)
+            {
+                //show help
+                showHelp();
+                return;
+            }
+
+            if (_args.Length == 1)
+            {
+                //show help
+                upload(_args[0].Trim().ToLower());
+                return;
+            }
+
+            //get first argument
+            switch (_args[0].Trim().ToLower())
+            {
+                case "config":
+                    config(_args, o);
+                    break;
+                case "upload":
+                    upload(_args, o);
+                    break;
+                case "status":
+                    status(_args, o);
+                    break;
+                default:
+                    showHelp();
+                    break;
+            }
+            return;
+        }
+
+        static private void upload(string FileName2Upload)
+        {
+            var ImgurClientID = Utility.getImgurClientID();
+            if (string.IsNullOrEmpty(ImgurClientID))
+            {
+                Console.WriteLine("Missing ImgurClientID.");
+                return;
+            }
+            // file exist?
+            if (!System.IO.File.Exists(FileName2Upload))
+            {
+                Console.WriteLine($"file {FileName2Upload} is not exist.");
+                return;
+            }
+            // check file type
+            var ext = System.IO.Path.GetExtension(FileName2Upload).ToLower();
+            if (ext != ".png" && ext != ".jpg" && ext != ".jepg" && ext != ".gif" && ext != ".mp4")
+            {
+                Console.WriteLine($"file type {ext} is invalid.");
+                return;
+            }
+            Console.WriteLine($"file {FileName2Upload} uploading...");
+
+            var FileBody = System.IO.File.ReadAllBytes(FileName2Upload);
+            var ret = Utility.UploadImage2Imgur(ImgurClientID, FileBody);
+            var JSON = Newtonsoft.Json.JsonConvert.SerializeObject(ret);
+            TextCopy.ClipboardService.SetText((string)ret.data.link);
+            Console.WriteLine($"status: file {FileName2Upload} has been uploaded to {ret.data.link}");
+        }
+
+        static private void upload(string[] args, Options o)
+        {
+            upload(o.FilePath);
+        }
+
+        //set up 
+        static private void config(string[] args, Options o)
+        {
+            //if has ImgurClientID
+            if (!string.IsNullOrEmpty(o.ImgurClientID))
+            {
+                //save to IsolatedStorage
+                var store = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication();
+                var file = store.OpenFile("ImgurClientID", FileMode.OpenOrCreate);
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.WriteLine(o.ImgurClientID);
+                    Console.WriteLine($"\n\nImgur ClientID has been updated : {o.ImgurClientID}\n");
+                    return;
+                }
+            }
+            else
+            {
+                showHelp();
+            }
+        }
+
+        //show status
+        static private void status(string[] args, Options o)
+        {
+            var rootDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Console.WriteLine($"Application Running Path : {rootDir}");
+            Console.WriteLine($"Imgur ClientID : {Utility.getImgurClientID()}");
+        }
+
+        static void showHelp()
+        {
+            showHelp(_ParserResult, null);
+        }
+
+        static void showHelp<T>(ParserResult<T> parserResult, IEnumerable<Error> errs)
+        {
+            var helpText = CommandLine.Text.HelpText.AutoBuild(parserResult, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                //h.Heading = "Myapp 2.0.0-beta"; //change header
+                //h.Copyright = "Copyright (c) 2019 Global.com"; //change copyright text
+                return CommandLine.Text.HelpText.DefaultParsingErrorsHandler(parserResult, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+
+            Console.WriteLine("Examples:");
+            Console.WriteLine("SnagitShare2Imgur config -i {Imgur App ClientID}");
+            Console.WriteLine("SnagitShare2Imgur upload -f {FilePath To Upload}");
+        }
+
+        static ParserResult<Options> _ParserResult = null;
+        static string[] _args;
         static void Main(string[] args)
         {
-            foreach (var item in args)
+            try
             {
-                Console.WriteLine($"\n{item}");
+                _args = args;
+                _ParserResult = Parser.Default.ParseArguments<Options>(args);
+                _ParserResult.WithParsed(RunOptions);
+                //.WithNotParsed(HandleParseError);
+                //.WithNotParsed(errs => showHelp(_ParserResult, errs));
             }
-            Console.ReadKey();
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message }\n");
+                showHelp();
+            }
+
+            return;
         }
+
     }
 }
--- Utility.cs
+++ Utility.cs
@@ -1,15 +1,37 @@
-  
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.IO;
 
-namespace isRock
+namespace SnagitShare2Imgur
 {
     public class Utility
     {
-        static dynamic UploadImage2Imgur(string ImgurClientID, byte[] FileBody)
+        static internal string getImgurClientID()
+        {
+            var store = System.IO.IsolatedStorage.IsolatedStorageFile.GetUserStoreForApplication();
+            if (!store.FileExists("ImgurClientID"))
+            {
+                // Console.WriteLine($"ImgurClientID is null.");
+                // Console.WriteLine($"\nplease use following command to setup Imgur ClientID");
+                // Console.WriteLine("line config --ImgurClientID [ImgurClientID]");
+                return null;
+            }
+            else
+            {
+                var file = store.OpenFile("ImgurClientID", FileMode.Open);
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    var words = reader.ReadToEnd();
+                    return words.Trim();
+                }
+            }
+        }
+
+        static internal dynamic UploadImage2Imgur(string ImgurClientID, byte[] FileBody)
         {
             HttpClient client = new HttpClient();
             string uriBase = "https://api.imgur.com/3/upload";
@@ -28,14 +50,12 @@ namespace isRock
                 // and "multipart/form-data".
                 content.Headers.ContentType =
                     new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-
                 // Asynchronously call the REST API method.
                 response = client.PostAsync(uri, content).Result;
             }
-
             // Asynchronously get the JSON response.
             string JSON = response.Content.ReadAsStringAsync().Result;
-
+            //DeserializeObject with dynamic data type 
             return Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(JSON);
         }
     }
